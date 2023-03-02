04dce538d2b25b0313b9c8af7d5a3f1347ceae76
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-03-02 16:45:49 +0300

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,6 @@
 import json
 import requests
 
-#def number_of_subscribers(subreddit):
- #   """Return the total number of subscribers on a given subreddit."""
-    #url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    #headers = {
-    #    "User-Agent": "Ahmed_belhaj"#"linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    #}
-   # response = requests.get(url, headers=headers, allow_redirects=False)
-   # if response.status_code == 404:
-  #      return 0
- #   results = response.json().get("data")
-#    return results.get("subscribers")
 
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
