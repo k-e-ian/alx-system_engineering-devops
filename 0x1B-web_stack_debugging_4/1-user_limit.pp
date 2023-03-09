417270932e96e251af329d94dca4b6e4cb2a3c56
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-03-09 23:40:17 +0300

    puppet-linter

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # Enable the user holberton to login and open files without error.
 
 # Increase hard file limit for Holberton user.
-exec { 'increase-hard-file-limit':
-  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile unlimited/g" /etc/security/limits.conf',
-  provider => 'shell'
+exec { 'Increase hard limit':
+    command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile unlimited/g" /etc/security/limits.conf',
+    provider => 'shell'
 }
 
 # Increase soft file limit for Holberton user.
-exec { 'increase-soft-file-limit':
-  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 3000/g" /etc/security/limits.conf',
-  provider => 'shell'
+exec { 'Increase soft limit':
+    command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 3000/g" /etc/security/limits.conf',
+    provider => 'shell'
 }
