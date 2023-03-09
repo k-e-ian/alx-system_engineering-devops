5045557a1535965b6797bc64ab634fa553e8a28f
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-03-09 23:25:39 +0300

    setup nginx server limits

--- 0x1B-web_stack_debugging_4/0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,8 @@
 # Increases the amount of traffic an Nginx server can handle.
 
 # Increase the ULIMIT of the default file
-exec { 'file limit':
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec { 'file':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
 }
