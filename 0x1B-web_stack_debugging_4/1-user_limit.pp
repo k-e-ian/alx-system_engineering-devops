b1d8518f8fd43305336ac0def3f5cb5ce0b93e7f
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-03-09 23:37:19 +0300

    pep 8

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 # Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit':
-	command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile unlimited/g" /etc/security/limits.conf',
-	provider => 'shell'
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile unlimited/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
 
 # Increase soft file limit for Holberton user.
 exec { 'increase-soft-file-limit':
-	command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 3000/g" /etc/security/limits.conf',
-    	provider => 'shell'
+  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 3000/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
